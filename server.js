7fd51b59e21071c2b65c84fc066679ea0402f405
Roopam Shukla <roopamscshukl@gmail.com>
2017-01-24 16:36:39 +0530

    Initialization

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-var TelegramBot = require('node-telegram-bot-api');
 
-// replace the value below with the Telegram token you receive from @BotFather
-var token = '302524771:AAHtssu38mpUt76FnX8Cg4g7AYZoJHnJ33I';
+telegramBot = require('node-telegram-bot-api');
 
-// Create a bot that uses 'polling' to fetch new updates
-var bot = new TelegramBot(token, { polling: true });
+var token = '';
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, function (msg, match) {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+var bot = new telegramBot(token, { polling: true });
 
-  var chatId = msg.chat.id;
-  var resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, "Received your message");
+  
+  if(msg.text=='/start')
+  {
+    bot.sendMessage(chatId,"Welcome");
+  }
+  else if(/[a-zA-Z0-9]/g.test(msg.text))
+  {
+    bot.sendMessage(chatId,"Shut Up 😋😌");
+  }
 });
